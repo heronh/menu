34394d290f3767d69b5662865a2d93ac8dac94f5
heronh <heronhurpia@gmail.com>
2025-09-09 11:05:04 -0300

    Criada versão inicial da página de cadastro

--- controllers/registerController.go
+++ controllers/registerController.go
@@ -1,11 +1,16 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "register.html", nil)
+	fmt.Println("Acessou a página de registro")
+	c.HTML(http.StatusOK, "register.html",
+		gin.H{
+			"Title": "Cadastro de Usuário",
+		})
 }
